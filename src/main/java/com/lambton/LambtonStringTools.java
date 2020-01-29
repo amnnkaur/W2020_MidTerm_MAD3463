686736b532e05f53eb4bc5f134717d1aa9eab3d4
macstudent <amankaur2628@gmail.com>
2020-01-29 18:38:29 -0500

    [Aman]variables for code 5

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -51,6 +51,15 @@ public class LambtonStringTools
     //5 - REPLACING SUBSTRING WITH NEW STRING IN EXISTING STRING
     public static String replaceSubString(String originalString, String findString, String newString) 
     {
+
+        originalString = originalString.toLowerCase();
+
+        findString = findString.toLowerCase();
+
+        newString = newString.toLowerCase();
+
+        
+
         return null;
     }
 }
